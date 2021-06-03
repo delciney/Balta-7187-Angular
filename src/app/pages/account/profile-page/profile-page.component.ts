22792826e31a6b2a8995e2bec3daebf74c43347a
Delciney <delciney07@gmail.com>
2021-06-03 14:26:26 -0300

    Edição de perfil parte 1

--- src/app/pages/account/profile-page/profile-page.component.ts
+++ src/app/pages/account/profile-page/profile-page.component.ts
@@ -1,4 +1,8 @@
+import { DataService } from 'src/app/services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CustomValidator } from 'src/app/validators/custom.validator';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  public busy = false;
+
+  constructor(
+    private service: DataService,
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) {
+    this.form = this.fb.group({
+      name: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(80),
+        Validators.required
+      ])],
+      document: [{ value: '', disabled: true }],
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(120),
+        Validators.required,
+        CustomValidator.EmailValidator
+      ])],
+    });
+  }
 
   ngOnInit(): void {
+    this.busy = true;
+    this
+      .service
+      .getProfile()
+      .subscribe(
+        (data: any) => {
+          this.busy = false;
+          this.form.controls['name'].setValue(data.name);
+          this.form.controls['document'].setValue(data.document);
+          this.form.controls['email'].setValue(data.email);
+        },
+        (err) => {
+          console.log(err);
+          this.busy = false;
+        }
+      );
+  }
+
+  submit() {
+    this.busy = true;
+    this
+      .service
+      .updateProfile(this.form.value)
+      .subscribe(
+        (data: any) => {
+          this.busy = false;
+          this.toastr.success(data.message, 'Atualização Completa!');
+        },
+        (err) => {
+          console.log(err);
+          this.busy = false;
+        }
+      );
   }
 
 }
