098909b584c71f1dd4286cd59a582d25ecd79edb
Delciney <delciney07@gmail.com>
2021-06-03 15:04:07 -0300

    Manipulando o carrinho

--- src/app/pages/store/cart-page/cart-page.component.ts
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
+  public cart: Cart = new Cart();
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loadCart();
+  }
+
+  public loadCart() {
+    this.cart = CartUtil.get();
+  }
+
+  public total() {
+    let total = 0;
+    this.cart.items.forEach((item) => {
+      total += (item.price * item.quantity);
+    });
+    return total;
+  }
+
+  public remove(item: any) {
+    let index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear() {
+    CartUtil.clear();
+    this.loadCart();
   }
 
 }
